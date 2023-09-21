9b7e7c896b96475dbc7a28c450090bb53ad884e5
lethicia1999 <lethiciaasevedo01@gmail.com>
2023-09-21 01:12:36 -0300

    calculando a quantia de produtos por loja

--- MeuArquivo.py
+++ MeuArquivo.py
@@ -11,6 +11,7 @@ pd.set_option('display.max_columns', None) #faz com que o terminal mostre todas 
 
 print(tabela_vendas)
 
+
 # faturamento por loja 
 faturamento = tabela_vendas [['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
 
@@ -18,7 +19,11 @@ print(faturamento)
 
 
 # quantidade de produtos vendidos por loja 
+quantidade = tabela_vendas [['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+
+print(quantidade)
 
 # ticket médio por produto em cada loja 
+ticket_medio = 
 
 # enviar um e-mail com relatório 
