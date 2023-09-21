a06268502cc32caa5e6ac8ae60b533e05e6884fa
lethicia1999 <lethiciaasevedo01@gmail.com>
2023-09-21 00:38:35 -0300

    importando a biblioteca pandas para leitura dos dados

---
+++ MeuArquivo.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+
+
+
+# importar a base de dados 
+
+# visualizar a base de dados 
+
+# faturamento por loja 
+
+# quantidade de produtos vendidos por loja 
+
+# ticket médio por produto em cada loja 
+
+# enviar um e-mail com relatório 
