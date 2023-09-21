9de2a7343fccdc6cfeef8212e782430380e7364f
lethicia1999 <lethiciaasevedo01@gmail.com>
2023-09-21 00:53:30 -0300

    aprendendo a visualizar a base de dados de forma completa

--- MeuArquivo.py
+++ MeuArquivo.py
@@ -5,11 +5,11 @@ import pandas as pd
 # importar a base de dados 
 tabela_vendas = pd.read_excel ('Vendas.xlsx')
 
-print(tabela_vendas)
-
-
 
 # visualizar a base de dados 
+pd.set_option('display.max_columns', None) #faz com que o terminal mostre todas as colunas sem cortes
+
+print(tabela_vendas)
 
 # faturamento por loja 
 
